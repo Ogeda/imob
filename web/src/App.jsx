aa3b06b23d886752528ef41e345e34a40604f33c
Brenno Ogeda <brennoogd@gmail.com>
2024-01-14 13:38:05 -0300

    init imobweb

---
+++ web/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Global from "./styles/Global";
+
+function App() {
+  return (
+    <div > 
+      <Header /> 
+      <Global />
+    </div>
+  );
+}
+
+export default App;
